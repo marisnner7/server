ad7361991298f6f00cbbb010713c31644095a4a9
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-09 23:23:15 -0300

    hot fixes axios

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,16 +1,12 @@
-import path from 'path'
-import 'dotenv/config'
+import path from 'path';
 
 module.exports = {
-  client: 'pg',
-  connection: {
-    host : process.env.PG_HOST,
-    user : process.env.PG_USER,
-    password : process.env.PG_PASSWORD,
-    database : process.env.PG_DATABASE,
-  },
-  migrations: {
-    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-  },
-  useNullAsDefault: true
-}
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+    },
+    migrations: {
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    useNullAsDefault: true,
+};
--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,15 +1,12 @@
-import knex from 'knex'
-import 'dotenv/config'
+import knex from 'knex';
+import path from 'path';
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    host : process.env.PG_HOST,
-    user : process.env.PG_USER,
-    password : process.env.PG_PASSWORD,
-    database : process.env.PG_DATABASE,
-  },
-  useNullAsDefault: true
-})
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'database.sqlite')
+    },
+    useNullAsDefault: true,
+});
 
-export default db
+export default db;
